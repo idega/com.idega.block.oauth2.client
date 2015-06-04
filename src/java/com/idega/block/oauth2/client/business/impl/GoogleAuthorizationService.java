55f237fa4a56b3e3320aacf7fdbc297ffa58d54a
lapiukshtiss <martynas.stake@gmail.com>
2015-06-04 15:12:43 +0300

    Getting credentials, which already used

--- src/java/com/idega/block/oauth2/client/business/impl/GoogleAuthorizationService.java
+++ src/java/com/idega/block/oauth2/client/business/impl/GoogleAuthorizationService.java
@@ -14,35 +14,42 @@ import org.springframework.stereotype.Service;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.idega.core.business.DefaultSpringBean;
 
 @Service(GoogleAuthorizationService.BEAN_NAME)
 @Scope(BeanDefinition.SCOPE_SINGLETON)
-public class GoogleAuthorizationService extends DefaultSpringBean{
+public class GoogleAuthorizationService extends DefaultSpringBean {
+
 	public static final String BEAN_NAME = "googleAuthorizationService";
+
 	public static final String SERVICE_NAME = "google";
-	
+
 	public static final JsonFactory JSON_FACTORY = new JacksonFactory();
-	
+
 	private Map<String, Credential> serverCredentials = new HashMap<String, Credential>();
-	
-	public Credential getServerAccountCredentials(String emailAddress,Collection<String> scopes,String pkPath) throws GeneralSecurityException, IOException{
+
+	public Credential getServerAccountCredentials(
+			String emailAddress,
+			Collection<String> scopes,
+			String pkPath) throws GeneralSecurityException, IOException {
 		Credential credential = serverCredentials.get(emailAddress);
-		if(credential != null){
-			return credential;
+		if(credential == null){
+			credential = new GoogleCredential.Builder()
+			    .setTransport(GoogleNetHttpTransport.newTrustedTransport())
+			    .setJsonFactory(JSON_FACTORY)
+			    .setServiceAccountId(emailAddress)
+			    .setServiceAccountPrivateKeyFromP12File(new File(pkPath))
+			    .setServiceAccountScopes(scopes)
+			    .build();
+			serverCredentials.put(emailAddress, credential);
 		}
-		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-		credential = new GoogleCredential.Builder()
-		    .setTransport(httpTransport)
-		    .setJsonFactory(JSON_FACTORY)
-		    .setServiceAccountId(emailAddress)
-		    .setServiceAccountPrivateKeyFromP12File(new File(pkPath))
-		    .setServiceAccountScopes(scopes)
-		    .build();
-		serverCredentials.put(emailAddress, credential);
+
 		return credential;
+	}
+
+	public Credential getCredential(String emailAddress) {
+		return serverCredentials.get(emailAddress);
 	}
 }
